fb21f96e7c5f0eb980222d14ca2ec238082b2972
Maxim Korinets <mkorinets@gmail.com>
2018-08-07 20:32:04 +0300

    Add misc improvements to the handler

--- modules/http/handler.js
+++ modules/http/handler.js
@@ -17,26 +17,29 @@ module.exports = async function (req, res) {
     const {method, url} = req;
     const trid = kojo.get('trid');
     const reqID = trid.seq();
-    logger.debug(`-> [${reqID}] ${method} ${url}`);
+    logger.debug(`<- [${reqID}] ${method} ${url}`);
     const start = process.hrtime();
     const {pathname} = URL.parse(url, true);
     const trimmedPath = pathname.replace(/\/+$/g, '');
     const routes = kojo.get('routes');
     res.sendDate = true;
+    res.setHeader('X-Request-ID', reqID);
 
     // 404 pathname not found
     if (!routes[trimmedPath]) {
-        logger.error(`!! [${reqID}]`, 'pathname not found');
-        res.writeHead(404);
-        res.end(http.STATUS_CODES[404]);
+        const statusCode = 404;
+        res.writeHead(statusCode);
+        res.end(http.STATUS_CODES[statusCode]);
+        logger.error(`!! [${reqID}] ${res.statusCode} ${http.STATUS_CODES[statusCode]}`);
         return
     }
 
     // 405 method not found (thus not allowed)
     if (routes[trimmedPath] && !routes[trimmedPath][method]) {
-        logger.error(`!! [${reqID}]`, 'method not found');
-        res.writeHead(405);
-        res.end(http.STATUS_CODES[405]);
+        const statusCode = 405;  // TODO abstract into some error response?
+        res.writeHead(statusCode);
+        res.end(http.STATUS_CODES[statusCode]);
+        logger.error(`!! [${reqID}] ${res.statusCode} ${http.STATUS_CODES[statusCode]}`);
         return
     }
 
@@ -46,7 +49,7 @@ module.exports = async function (req, res) {
         const JSONstring = JSON.stringify(await routeHandler(req, res));
         const length = Buffer.byteLength(JSONstring);
         res.setHeader('Content-Length', length);
-        logger.debug(`<- [${reqID}] ${res.statusCode} / ${length}bytes / ${HRT2sec(process.hrtime(start))}sec`);
+        logger.debug(`-> [${reqID}] ${res.statusCode} / ${length} bytes / ${HRT2sec(process.hrtime(start))} sec`);
         res.end(JSONstring);
     } catch (error) {
         logger.error(`!! [${reqID}]`, error);
